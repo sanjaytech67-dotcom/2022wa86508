3d2f719cd7224d46afe3301c89808fd803541728
sanjaytech67-dotcom <sanjay.tech67@gmail.com>
2026-09-22 17:31:15 +0530

    Add initial App class with main method

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,8 @@
+package com.bits;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Lab - BITS ID: 2022wa86508");
+        System.out.println("Continuous Build and Continuous Integration using Jenkins + Maven + Git");
+    }
+}
